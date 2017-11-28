c6753bff8c2308cfb8ed7336ba3c549646e28a29
Unknown <eolsurion369@hotmail.com>
2017-11-28 11:09:33 -0800

    Exercise 2 further revisions

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -1,12 +1,7 @@
 namespace BackEndFoundationsCSharp.Models {
     public class Character {
-        public Character () {
-            FirstName = "Jon";
-            LastName = "Snow";
-        }
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = "Jon";
+        public string LastName { get; set; } = "Snow";
     }
 
 }
